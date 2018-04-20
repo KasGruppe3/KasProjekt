154a6145303b458a0850bf79b1888c8e0268258c
kennetn <kennetn@10.24.2.182>
2018-04-20 10:05:07 +0200

    extra færdig, hotel midlertidig

--- src/model/Hotel.java
+++ src/model/Hotel.java
@@ -1,8 +1,65 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Hotel {
     private String name;
     private String address;
     private double priceSingle;
     private double priceDouble;
+    private ArrayList<Extra> extraOffers = new ArrayList<Extra>();
+    
+    public Hotel (String name, String address, double priceSingle, double priceDouble) {
+    	this.name = name;
+    	this.address = address;
+    	this.priceSingle = priceSingle;
+    	this.priceDouble = priceDouble;
+    }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public double getPriceSingle() {
+		return priceSingle;
+	}
+
+	public void setPriceSingle(double priceSingle) {
+		this.priceSingle = priceSingle;
+	}
+
+	public double getPriceDouble() {
+		return priceDouble;
+	}
+
+	public void setPriceDouble(double priceDouble) {
+		this.priceDouble = priceDouble;
+	}
+    
+    public Extra addExtra(String name, double price, String description){
+    	Extra extra = new Extra(name, price, description);
+    	extraOffers.add(extra);
+    	return extra;
+    }
+    
+    public ArrayList<Extra> getExtra() {
+    	Arraylist<Extra> extra = extraOffers.clone();
+    	return extra;
+    }
+    
+    public void removeExtra(Extra extra) {
+    	extraOffers.remove(extra);
+    }
 }
