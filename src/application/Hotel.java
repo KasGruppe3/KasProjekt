117cc549918fd2c9ea674a0fce3fc2a473b64905
Kenneth Gorking <dasapfe@yahoo.dk>
2018-04-26 11:32:43 +0200

    Tilføjede kommentarer

--- src/application/Hotel.java
+++ src/application/Hotel.java
@@ -2,6 +2,10 @@ package application;
 
 import java.util.ArrayList;
 
+/**
+ * A class describing a hotel attendants can stay at.
+ *
+ */
 public class Hotel {
     private String name;
     private String address;
@@ -9,6 +13,18 @@ public class Hotel {
     private double priceDouble;
     private ArrayList<Extra> extraOffers = new ArrayList<>();
 
+    /**
+     * Create a hotel instance
+     *
+     * @param name
+     *            The name of the hotel
+     * @param address
+     *            Its address
+     * @param priceSingle
+     *            Price of a single room
+     * @param priceDouble
+     *            Price of a double room
+     */
     public Hotel(String name, String address, double priceSingle, double priceDouble) {
         this.name = name;
         this.address = address;
@@ -48,6 +64,18 @@ public class Hotel {
         this.priceDouble = priceDouble;
     }
 
+    /**
+     * Adds an extra service the hotel offers its guests, like wifi, breakfast,
+     * etc.
+     * 
+     * @param name
+     *            Name of the extra service
+     * @param price
+     *            Price of the service
+     * @param description
+     *            Description of the service
+     * @return An Extra instance
+     */
     public Extra addExtra(String name, double price, String description) {
         Extra extra = new Extra(name, price, description);
         extraOffers.add(extra);
