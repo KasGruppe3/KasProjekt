15eef1d4ff769c0471568e8e360c3651b43ec1e0
kennetn <kennetn@10.24.2.182>
2018-04-26 11:45:04 +0200

    nej

--- src/application/Companion.java
+++ src/application/Companion.java
@@ -1,14 +1,14 @@
 package application;
 
 public class Companion extends Person {
-    private Attendant attendant;
+	private Attendant attendant;
 
-    public Companion(String name, Attendant attendant) {
-        super(name);
-        this.attendant = attendant;
-    }
+	public Companion(String name, Attendant attendant) {
+		super(name);
+		this.attendant = attendant;
+	}
 
-    public Attendant getAttendant() {
-        return attendant;
-    }
+	public Attendant getAttendant() {
+		return attendant;
+	}
 }
