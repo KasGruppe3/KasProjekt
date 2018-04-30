3a1411ee494cbbf2333000902bd6c7d4d14a4ff3
kennetn <kennetn@10.24.2.182>
2018-04-30 12:59:05 +0200

    færdig med attendanttab

--- src/gui/AttendantTab.java
+++ src/gui/AttendantTab.java
@@ -1,10 +1,8 @@
 package gui;
 
 import javafx.scene.control.TextField;
-import application.Attendant;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.GridPane;
 
@@ -21,43 +19,47 @@ public class AttendantTab extends Tab {
         pane.setVgap(10);
 
         setContent(pane);
+        setClosable(false);
 		
 		Label nameLabel = new Label("Navn:");
-		pane.add(nameLabel, 1, 1);
+		pane.add(nameLabel, 0, 0);
 		
 		TextField txfName = new TextField();
         txfName = new TextField();
         txfName.setEditable(true);
-        pane.add(txfName, 2, 1);
+        pane.add(txfName, 1, 0);
 		
         Label addressLabel = new Label("Adresse:");
-		pane.add(addressLabel, 1, 2);
+		pane.add(addressLabel, 0, 1);
 		
 		TextField txfAddress = new TextField();
 		txfAddress = new TextField();
 		txfAddress.setEditable(true);
-        pane.add(txfAddress, 2, 2);
+        pane.add(txfAddress, 1, 1);
+        
+        GridPane tlfPane = new GridPane();
+        pane.add(tlfPane, 1, 2);
         
         Label tlfLabel = new Label("Tlf:");
-		pane.add(tlfLabel, 1, 3);
+		pane.add(tlfLabel, 0, 2);
 		
 		TextField txfCountryCode = new TextField();
 		txfCountryCode = new TextField();
 		txfCountryCode.setEditable(true);
         txfCountryCode.setMaxWidth(50.0);
-		pane.add(txfCountryCode, 2, 3);
+		tlfPane.add(txfCountryCode, 1, 0);
         
         TextField txfTlfNummer = new TextField();
         txfTlfNummer = new TextField();
         txfTlfNummer.setEditable(true);
-        pane.add(txfTlfNummer, 3, 3);
+        tlfPane.add(txfTlfNummer, 2, 0);
         
         Label companyLabel = new Label("Firma:");
-		pane.add(companyLabel, 1, 4);
+		pane.add(companyLabel, 0, 3);
 		
 		TextField txfCompany = new TextField();
 		txfCompany = new TextField();
 		txfCompany.setEditable(true);
-        pane.add(txfCompany, 2, 4);
+        pane.add(txfCompany, 1, 3);
     }
 }
