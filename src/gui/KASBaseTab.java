58db402e94481086fdc7b98e31f4f7bf0deca5ea
Kenneth Gorking <dasapfe@yahoo.dk>
2018-04-30 11:02:42 +0200

    Opdateret kode

--- src/gui/KASBaseTab.java
+++ src/gui/KASBaseTab.java
@@ -6,8 +6,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 
 public class KASBaseTab extends Tab {
+
+    private Label fieldMessage;
 
     public KASBaseTab() {
         super();
@@ -51,12 +54,42 @@ public class KASBaseTab extends Tab {
         return dp;
     }
 
+    protected Label addErrorMessageLabel(GridPane pane, int row, int columnSpan) {
+        Label label = createLabel("hidden error msg");
+        pane.add(label, 0, row, columnSpan, 1);
+        return label;
+    }
+
     private Label createLabel(String text) {
         Label label = new Label();
         label.setText(text);
         // label.setMaxWidth(300);
         label.setMinWidth(150);
         return label;
+    }
+
+    protected void createMessageField(GridPane pane, int column, int row, int columnSpan) {
+        fieldMessage = createLabel("hidden error msg");
+        pane.add(fieldMessage, column, row, columnSpan, 1);
+        fieldMessage.setVisible(false);
+    }
+
+    protected void hideMessage() {
+        fieldMessage.setVisible(false);
+    }
+
+    protected void showMessage(String text, Color color) {
+        fieldMessage.setTextFill(color);
+        fieldMessage.setText(text);
+        fieldMessage.setVisible(true);
+    }
+
+    protected void showError(String text) {
+        showMessage(text, Color.RED);
+    }
+
+    protected void showInformation(String text) {
+        showMessage(text, Color.GREEN);
     }
 
 }
