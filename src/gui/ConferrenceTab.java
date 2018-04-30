c0717a6651966bf20c8f54d1386fdfa21568b5dc
kennetn <kennetn@10.24.2.182>
2018-04-30 13:55:41 +0200

    lavet færdig

--- src/gui/ConferrenceTab.java
+++ src/gui/ConferrenceTab.java
@@ -1,7 +1,9 @@
 package gui;
 
 import application.Conference;
+import application.Hotel;
 import application.Service;
+import javafx.beans.value.ChangeListener;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -12,6 +14,15 @@ import javafx.scene.layout.GridPane;
 
 	public class ConferrenceTab extends Tab {
 
+		private ListView<Conference> conferenceList = new ListView<Conference>();
+		
+		private TextField txfName = new TextField();
+		private TextField txfAddress = new TextField();
+		private TextField txfPris = new TextField();
+		TextField txfStartDate = new TextField();
+		private TextField txfTimeSpan = new TextField();
+		
+		
 	    public ConferrenceTab() {
 	        super("Vælg konferrence");
 
@@ -26,7 +37,7 @@ import javafx.scene.layout.GridPane;
 	        Label conferrenceListLabel = new Label("Konferrencer:");
 			pane.add(conferrenceListLabel, 0, 0);
 			
-			ListView<Conference> conferenceList = new ListView<Conference>();
+			
 			pane.add(conferenceList, 0, 1,1,5);
 			conferenceList.setMaxHeight(120);
 			
@@ -35,7 +46,7 @@ import javafx.scene.layout.GridPane;
 			Label nameLabel = new Label("Navn:");
 			pane.add(nameLabel, 1, 0);
 			
-			TextField txfName = new TextField();
+			
 	        txfName = new TextField();
 	        txfName.setEditable(false);
 	        pane.add(txfName, 2, 0);
@@ -43,7 +54,7 @@ import javafx.scene.layout.GridPane;
 	        Label addressLabel = new Label("Adresse:");
 			pane.add(addressLabel, 1, 1);
 			
-			TextField txfAddress = new TextField();
+			
 			txfAddress = new TextField();
 			txfAddress.setEditable(false);
 	        pane.add(txfAddress, 2, 1);
@@ -51,7 +62,7 @@ import javafx.scene.layout.GridPane;
 	        Label priceLabel = new Label("Pris:");
 			pane.add(priceLabel, 1, 2);
 	        
-	        TextField txfPris = new TextField();
+	        
 	        txfPris = new TextField();
 	        txfPris.setEditable(false);
 	        pane.add(txfPris, 2, 2);
@@ -59,7 +70,7 @@ import javafx.scene.layout.GridPane;
 	        Label startDateLabel = new Label("Startdato:");
 			pane.add(startDateLabel, 1, 3);
 			
-			TextField txfStartDate = new TextField();
+			
 			txfStartDate = new TextField();
 			txfStartDate.setEditable(false);
 	        pane.add(txfStartDate, 2, 3);
@@ -67,9 +78,21 @@ import javafx.scene.layout.GridPane;
 	        Label timeSpanLabel = new Label("Forløbstid:");
 			pane.add(timeSpanLabel, 1, 4);
 			
-			TextField txfTimeSpan = new TextField();
+			
 			txfTimeSpan = new TextField();
 			txfTimeSpan.setEditable(false);
 	        pane.add(txfTimeSpan, 2, 4);
+	        
+	        ChangeListener<Conference> listener = (ov, oldConferrence, newConferrence) -> this.UpdateTextFields();
+	        conferenceList.getSelectionModel().selectedItemProperty().addListener(listener);
+	    }
+	    
+	    public void UpdateTextFields() {
+	    	Conference conference = conferenceList.getSelectionModel().getSelectedItem();
+	    	this.txfName.setText(conference.getName());
+	    	this.txfAddress.setText(conference.getLocation());
+	    	this.txfPris.setText("" + conference.getPrice());
+	    	this.txfStartDate.setText(conference.getDate().toString());
+	    	this.txfTimeSpan.setText("" + conference.getTimeSpan());
 	    }
 	}
