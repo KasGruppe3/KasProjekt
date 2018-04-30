32f458f45a776a50a9ca39bd27d8512b6024adfe
kennetn <kennetn@10.24.2.182>
2018-04-30 12:50:10 +0200

    opdateret hotelTab

--- src/gui/HotelTab.java
+++ src/gui/HotelTab.java
@@ -4,9 +4,7 @@ import application.Extra;
 import application.Hotel;
 import application.Service;
 import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
@@ -37,14 +35,14 @@ public class HotelTab extends Tab {
 		Label hotelListLabel = new Label("Hoteller:");
 		pane.add(hotelListLabel, 0, 0);
 
-
+		hotelList.getItems().setAll(Service.getHotels());
 		
 		pane.add(hotelList, 0, 1);
 
 		ChangeListener<Hotel> listener = (ov, oldHotel, newHotel) -> this.updateExtras();
 		hotelList.getSelectionModel().selectedItemProperty().addListener(listener);
-
-
+		
+		
 
 		Label hotelExtrasLabel = new Label("Ekstra tilvalg");
 		pane.add(hotelExtrasLabel, 1, 0);
@@ -61,5 +59,7 @@ public class HotelTab extends Tab {
 		Hotel hotel = hotelList.getSelectionModel().getSelectedItem();
 		hotelExtras.getItems().setAll(hotel.getExtras());
 	}
+	
+	
 
 }
