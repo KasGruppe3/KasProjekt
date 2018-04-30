eb5cb67a8e6301626f39d6715c66677a8197132b
Kenneth Gorking <dasapfe@yahoo.dk>
2018-04-30 14:37:14 +0200

    plob

--- src/gui/AttendantTab.java
+++ src/gui/AttendantTab.java
@@ -1,16 +1,24 @@
 package gui;
 
 import javafx.scene.control.TextField;
+
+import java.util.Hashtable;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.GridPane;
 
 public class AttendantTab extends Tab {
-	
+
+    private TextField txfName;
+    private TextField txfAddress;
+    private TextField txfCountryCode;
+    private TextField txfTlfNummer;
+    private TextField txfCompany;
 
     public AttendantTab() {
-        super("Vælg deltager");
+        super("VÃ¦lg deltager");
 
         GridPane pane = new GridPane();
         pane.setGridLinesVisible(false);
@@ -20,46 +28,61 @@ public class AttendantTab extends Tab {
 
         setContent(pane);
         setClosable(false);
-		
-		Label nameLabel = new Label("Navn:");
-		pane.add(nameLabel, 0, 0);
-		
-		TextField txfName = new TextField();
+
+        Label nameLabel = new Label("Navn:");
+        pane.add(nameLabel, 0, 0);
+
         txfName = new TextField();
         txfName.setEditable(true);
         pane.add(txfName, 1, 0);
-		
+
         Label addressLabel = new Label("Adresse:");
-		pane.add(addressLabel, 0, 1);
-		
-		TextField txfAddress = new TextField();
-		txfAddress = new TextField();
-		txfAddress.setEditable(true);
+        pane.add(addressLabel, 0, 1);
+
+        txfAddress = new TextField();
+        txfAddress.setEditable(true);
         pane.add(txfAddress, 1, 1);
-        
+
         GridPane tlfPane = new GridPane();
         pane.add(tlfPane, 1, 2);
-        
+
         Label tlfLabel = new Label("Tlf:");
-		pane.add(tlfLabel, 0, 2);
-		
-		TextField txfCountryCode = new TextField();
-		txfCountryCode = new TextField();
-		txfCountryCode.setEditable(true);
+        pane.add(tlfLabel, 0, 2);
+
+        txfCountryCode = new TextField();
+        txfCountryCode.setEditable(true);
         txfCountryCode.setMaxWidth(50.0);
-		tlfPane.add(txfCountryCode, 1, 0);
-        
-        TextField txfTlfNummer = new TextField();
+        tlfPane.add(txfCountryCode, 1, 0);
+
         txfTlfNummer = new TextField();
         txfTlfNummer.setEditable(true);
         tlfPane.add(txfTlfNummer, 2, 0);
-        
+
         Label companyLabel = new Label("Firma:");
-		pane.add(companyLabel, 0, 3);
-		
-		TextField txfCompany = new TextField();
-		txfCompany = new TextField();
-		txfCompany.setEditable(true);
+        pane.add(companyLabel, 0, 3);
+
+        txfCompany = new TextField();
+        txfCompany.setEditable(true);
         pane.add(txfCompany, 1, 3);
+    }
+
+    public String getName() {
+        return txfName.getText();
+    }
+
+    public String getAddress() {
+        return txfAddress.getText();
+    }
+
+    public String getCountryCode() {
+        return txfCountryCode.getText();
+    }
+
+    public String getTlfNummer() {
+        return txfTlfNummer.getText();
+    }
+
+    public String getCompany() {
+        return txfCompany.getText();
     }
 }
